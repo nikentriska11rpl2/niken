5a42b83779723371119034625a57dc51a38d73f7
nikentriska11rpl2 <nikentriska49@gmail.com>
2022-05-13 10:31:18 +0700

    first commit

---
+++ app/Controllers/TesController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controllers;
+
+use CodeIgniter\Controller;
+
+
+class TesController extends Controller{
+    public function index()
+    {
+        return view(TesView);
+    }
+
+}
